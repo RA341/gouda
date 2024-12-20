a7d7ef7bea4a2234bb83abf16e4a68ae0fd5f76c
RA341 <razerishi1505@gmail.com>
2024-12-20 11:30:17 -0500

    fix: db logging only on debug

--- src/service/database.go
+++ src/service/database.go
@@ -6,18 +6,24 @@ import (
 	"gorm.io/driver/sqlite" // Sqlite driver based on CGO
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"os"
 )
 
 func InitDB(dbPath string) (*gorm.DB, error) {
 	// Configure SQLite to use WAL mode
-	dialector := sqlite.Open(dbPath + "?_journal_mode=WAL&_busy_timeout=5000")
+	connectionStr := sqlite.Open(dbPath + "?_journal_mode=WAL&_busy_timeout=5000")
 
 	config := &gorm.Config{
-		Logger:      logger.Default.LogMode(logger.Info),
-		PrepareStmt: true, // Cache prepared statements
+		PrepareStmt: true,
+	}
+	if os.Getenv("DEBUG") == "true" {
+		config = &gorm.Config{
+			Logger:      logger.Default.LogMode(logger.Info),
+			PrepareStmt: true,
+		}
 	}
 
-	db, err := gorm.Open(dialector, config)
+	db, err := gorm.Open(connectionStr, config)
 	if err != nil {
 		return nil, err
 	}
