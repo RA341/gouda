7f3b19c9ca07ae1d7ccae61cf8d6088fff45295a
RA341 <razerishi1505@gmail.com>
2025-03-24 13:53:17 -0400

    added logout

--- brie/lib/ui/components/sidebar.dart
+++ brie/lib/ui/components/sidebar.dart
@@ -1,3 +1,5 @@
+import 'package:brie/config.dart';
+import 'package:brie/grpc/api.dart';
 import 'package:brie/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,28 +14,50 @@ class VerticalNavBar extends ConsumerWidget {
     final pageIndex = ref.watch(pageIndexListProvider);
     final labelType = NavigationRailLabelType.all;
 
-    return NavigationRail(
-      selectedIndex: pageIndex,
-      groupAlignment: -0.955,
-      onDestinationSelected: (int index) =>
-          ref.read(pageIndexListProvider.notifier).navigateToPage(index),
-      labelType: labelType,
-      leading: GoudaHeader(),
-      destinations: [
-        NavigationRailDestination(
-          icon: Icon(Icons.home_outlined, size: iconsSize),
-          selectedIcon: Icon(Icons.home_filled, size: iconsSize),
-          label: Text('Home'),
+    return Column(
+      children: [
+        Expanded(
+          child: NavigationRail(
+            selectedIndex: pageIndex,
+            groupAlignment: -0.955,
+            onDestinationSelected: (int index) =>
+                ref.read(pageIndexListProvider.notifier).navigateToPage(index),
+            labelType: labelType,
+            leading: GoudaHeader(),
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home_outlined, size: iconsSize),
+                selectedIcon: Icon(Icons.home_filled, size: iconsSize),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.book_outlined, size: iconsSize),
+                selectedIcon: Icon(Icons.book, size: iconsSize),
+                label: Text('Categories'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings_outlined, size: iconsSize),
+                selectedIcon: Icon(Icons.settings, size: iconsSize),
+                label: Text('Settings'),
+              ),
+            ],
+          ),
         ),
-        NavigationRailDestination(
-          icon: Icon(Icons.book_outlined, size: iconsSize),
-          selectedIcon: Icon(Icons.book, size: iconsSize),
-          label: Text('Categories'),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.settings_outlined, size: iconsSize),
-          selectedIcon: Icon(Icons.settings, size: iconsSize),
-          label: Text('Settings'),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          child: IconButton(
+            onPressed: () async {
+              await prefs.setString('apikey', '');
+              ref.invalidate(apiTokenProvider);
+            },
+            icon: Tooltip(
+              message: 'Logout',
+              child: Icon(
+                Icons.logout,
+                semanticLabel: 'Logout',
+              ),
+            ),
+          ),
         ),
       ],
     );
