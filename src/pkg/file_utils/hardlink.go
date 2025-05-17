5291d5400db101beb08899c6b2ad74da30037e89
RA341 <razerishi1505@gmail.com>
2025-05-17 18:41:43 -0400

    added copy file/folder

--- src/pkg/file_utils/hardlink.go
+++ src/pkg/file_utils/hardlink.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
-	"mime"
-	"net/http"
 	"os"
-	"path"
 	"path/filepath"
 	"runtime"
 )
@@ -37,9 +34,17 @@ func RecursiveChown(path string, uid, gid int) error {
 	})
 }
 
+func CopyFolder(sourceDir, targetDir string) error {
+	return walkFolder(sourceDir, targetDir, copyFile)
+}
+
 // HardLinkFolder creates hard links of all files from source folder to target folder
 // and maintains the original UID/GID
 func HardLinkFolder(sourceDir, targetDir string) error {
+	return walkFolder(sourceDir, targetDir, hardlinkFile)
+}
+
+func walkFolder(sourceDir, targetDir string, actionFunc func(oldPath string, newPath string, mode fs.FileMode) error) error {
 	// Check if source directory exists
 	sourceStat, err := os.Stat(sourceDir)
 	if err != nil {
@@ -59,8 +64,8 @@ func HardLinkFolder(sourceDir, targetDir string) error {
 		targetDir = fmt.Sprintf("%s/%s", targetDir, sourceFile)
 
 		// Create hard link for the file
-		if err := createHardLink(sourceDir, targetDir, sourceStat.Mode()); err != nil {
-			return fmt.Errorf("failed to create hard link from %s to %s: %w", sourceDir, targetDir, err)
+		if err := actionFunc(sourceDir, targetDir, sourceStat.Mode()); err != nil {
+			return err
 		}
 
 		return nil
@@ -97,78 +102,74 @@ func HardLinkFolder(sourceDir, targetDir string) error {
 		}
 
 		// Create hard link for files
-		err = createHardLink(path, targetPath, info.Mode())
+		err = actionFunc(path, targetPath, info.Mode())
 		if err != nil {
-			return fmt.Errorf("failed to create hard link from %s to %s: %w", path, targetPath, err)
+			return err
 		}
 
 		return nil
 	})
 }
 
-func DownloadTorrentFile(downloadLink, downloadPath string) (string, error) {
-	resp, err := http.Get(downloadLink)
+func copyFile(src, dest string, mode fs.FileMode) error {
+	sourceFile, err := os.Open(src)
 	if err != nil {
-		return "", fmt.Errorf("failed to make request: %v", err)
+		return fmt.Errorf("failed to open source file %s: %w", src, err)
 	}
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("invalid http code: %d, reason: %s", resp.StatusCode, resp.Status)
-	}
+	defer func(sourceFile *os.File) {
+		_ = sourceFile.Close()
+	}(sourceFile)
 
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
-
-	// Get filename from Content-Disposition header
-	_, params, err := mime.ParseMediaType(resp.Header.Get("Content-Disposition"))
-	filename := ""
-	if err == nil && params["filename"] != "" {
-		filename = params["filename"]
-	} else {
-		// Fallback to URL path if header not available
-		filename = path.Base(downloadLink)
-	}
-
-	if err := os.MkdirAll(downloadPath, 0775); err != nil {
-		return "", fmt.Errorf("failed to create directories: %v", err)
-	}
-
-	destPath := fmt.Sprintf("%s/%s", downloadPath, filename)
-	file, err := os.Create(destPath)
+	// Get source file info for permissions
+	sourceInfo, err := sourceFile.Stat()
 	if err != nil {
-		return "", fmt.Errorf("failed to create file: %v", err)
+		return fmt.Errorf("failed to stat source file %s: %w", src, err)
 	}
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
 
-	_, err = io.Copy(file, resp.Body)
+	// Create the destination file for writing.
+	// If it exists, it will be truncated.
+	destinationFile, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE|os.O_TRUNC, sourceInfo.Mode())
 	if err != nil {
-		return "", fmt.Errorf("failed to copy response body: %v", err)
+		return fmt.Errorf("failed to create destination file %s: %w", dest, err)
+	}
+	defer func(destinationFile *os.File) {
+		_ = destinationFile.Close()
+	}(destinationFile)
+
+	// Copy the contents from source to destination
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return fmt.Errorf("failed to copy data from %s to %s: %w", src, dest, err)
 	}
 
-	return destPath, nil
+	// Ensure all data is written to stable storage
+	err = destinationFile.Sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync destination file %s: %w", dest, err)
+	}
+
+	return nil
 }
 
-// createHardLink creates a hard link with proper ownership and permissions
-func createHardLink(oldPath, newPath string, mode fs.FileMode) error {
+// hardlinkFile creates a hard link with proper ownership and permissions
+func hardlinkFile(src, dest string, mode fs.FileMode) error {
 	// Ensure the target directory exists
-	targetDir := filepath.Dir(newPath)
+	targetDir := filepath.Dir(dest)
 	if err := os.MkdirAll(targetDir, 0755); err != nil {
 		return err
 	}
 
 	// Remove existing file if it exists
-	if err := os.Remove(newPath); err != nil && !os.IsNotExist(err) {
+	if err := os.Remove(dest); err != nil && !os.IsNotExist(err) {
 		return err
 	}
 
 	// Create the hard link
-	if err := os.Link(oldPath, newPath); err != nil {
+	if err := os.Link(src, dest); err != nil {
 		return err
 	}
 
-	if err := os.Chmod(newPath, mode); err != nil {
+	if err := os.Chmod(dest, mode); err != nil {
 		return fmt.Errorf("failed to set permissions: %w", err)
 	}
 
